5722b62f4c49f0400d1d53c364395592c6ad7933
Melina Bustos <melina.m.bustos@gmail.com>
2025-09-03 21:05:56 -0300

    practica clase 2

---
+++ TestingAutomatizacion/Clase 2/practica.py
@@ -0,0 +1,23 @@
+# comentario
+
+edad = 23
+altura = 1.71
+nombre = "Jo"
+es_de_dia = False
+
+x, y, z = 1, 2, 3
+
+print(x)
+print("bbb" + str(45))# uso str para convertir el nombre en string
+
+edad = (int("Ingresa la edad"))#el input me devuelve string por eso lo convierto en int
+print(type(edad))
+
+
+mayoria_edad = edad >= 10
+edad.isdigit()# va a dar true o false si es numero o no
+# if 
+if edad >= 18: 
+    print(nombre + "Es mayor de edad")
+else:
+    print(nombre + "Es menor de edad")
