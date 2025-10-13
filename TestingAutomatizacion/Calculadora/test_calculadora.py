4e653eb22bef63bc8017d37db04df63f59321367
Melina <melina.m.bustos@gmail.com>
2025-10-13 12:37:27 -0300

    clase de consulta clase 4

--- TestingAutomatizacion/Calculadora/test_calculadora.py
+++ TestingAutomatizacion/Calculadora/test_calculadora.py
@@ -8,8 +8,59 @@ from calculadora import suma, resta, division, multiplicacion # importo las func
 #    assert  suma(2, 3) == 5 #nos va a permitir validar la prueba con datos de entrada y datos de salida
 #    assert suma(5, 3) == 40
 #-----------------------------------------------------------------
-#parametrizacion tambien es una etiqueta
+#fixture se preparan datos que luego voy a reutilizar
+#los fixtures se van a declarar antes que las funciones a testear
+@pytest.fixture
+def numero ():
+    return [1,2,3]
 
+#se pueden desempaquetar los datos que puse en la funcion numero
+def test_suma1(numero):
+    a,b, c = numero
+    assert a + b + c  == 6
+
+#ver porque da error
+#def test_suma2(numero):
+ #   assert suma(numero) == 6
+
+# el fixture tambien se puede aplicar a un ciclo
+@pytest.fixture
+def numeros():
+    return [(1,2), (2,3), (5,2)]
+
+def test_suma3(numeros):
+    for a,b in numeros:
+        resultado = a + b
+        assert resultado == a + b
+
+#otra forma de arreglo de datos
+@pytest.fixture
+def datos_entrada():
+    return {
+        "caso_positivo": (2,3),
+        "caso_cero": (0,5),
+        "caso_negativo": (-1,7)
+    }
+@pytest.mark.parametrize("clave, resultado_esperado", [
+    ("caso_positivo",5),
+    ("caso_cero", 5),
+    ("caso_negativo", 6)
+])
+def test_suma4(datos_entrada, clave, resultado_esperado):
+    a,b = datos_entrada[clave]
+    assert suma(a, b) == resultado_esperado
+
+@pytest.mark.parametrize("clave, resultado_esperado", [
+    ("caso_positivo",-1),
+    ("caso_cero", -5),
+    ("caso_negativo", -8)
+])
+def test_resta2(datos_entrada, clave, resultado_esperado):
+    a,b = datos_entrada[clave]
+    assert resta(a, b) == resultado_esperado
+
+#--------------------------------------------------------------------
+#parametrizacion tambien es una etiqueta
 @pytest.mark.criticos
 @pytest.mark.parametrize("a,b,resultado", [
     (10,5,15),
